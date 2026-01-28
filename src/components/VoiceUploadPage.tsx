c48ea985b0df0f169bff83b3ba6a886d17f89bc9
Mokhtar Mohammadi <187335232+MR-MOKHTAR@users.noreply.github.com>
2026-01-28 12:30:23 +0300

    ...

--- src/components/VoiceUploadPage.tsx
+++ src/components/VoiceUploadPage.tsx
@@ -361,7 +361,6 @@ export function VoiceUploadPage() {
           <input
             id="file-upload"
             type="file"
-            accept=".mp3,.wav,.ogg,.m4a"
             className="hidden"
             onChange={handleFileSelect}
           />
